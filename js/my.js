8a2c52ebee4fd13062edd6e4c0b3c5a196368d08
Dustin Matura <duskrmatura@addu.edu.ph>
2021-03-27 18:14:06 +0800

    Final Commit

--- js/my.js
+++ js/my.js
@@ -1,42 +1,45 @@
 var subtotal = document.getElementById("subtotal");
 var tax = document.getElementById("tax");
 var total = document.getElementById("total");
-var paid = document.getElementById("paid");
+var aPaid = document.getElementById("aPaid");
 var change = document.getElementById("change");
-var pButton = document.getElementById("pButton");
+var submit = document.getElementById("submit");
 subtotal.min = 100;
 
 subtotal.addEventListener("change", function(){
     if(parseInt(subtotal.value) >= 100 && parseInt(subtotal.value) <= 10000)
     {
-        pButton.style.backgroundColor = "green";
-        paid.disabled = false;
-        pButton.disabled = false;
+        submit.style.backgroundColor = "green";
+        aPaid.disabled = false;
+        submit.disabled = false;
         tax.value = (subtotal.value * 0.12);
         total.value = parseInt(tax.value) + parseInt(subtotal.value);
-        paid.min = total.value;
-        paid.value = total.value;
-        change.value = parseInt(paid.value) - parseInt(total.value);
+        aPaid.min = total.value;
+        aPaid.value = total.value;
+        change.value = parseInt(aPaid.value) - parseInt(total.value);
     }
+    
     else
     {
-        pButton.style.backgroundColor = rgb(192, 192, 192);
-        paid.disabled = true;
-        pButton.disabled = true;
+        submit.style.backgroundColor = "#8b0000";
+        aPaid.disabled = true;
+        submit.disabled = true;
     }
 })
 
-paid.addEventListener("change", function(){
-    change.value = parseInt(paid.value) - parseInt(total.value);
+aPaid.addEventListener("change", function(){
+    change.value = parseInt(aPaid.value) - parseInt(total.value);
     
     if(parseInt(change.value) >= 0)
     {
-        pButton.style.backgroundColor = rgb(120, 173, 80)
-        pButton.disabled = false;
+        submit.style.backgroundColor = "green";
+        submit.disabled = false;
     }
+
     else
     {
-        pButton.style.backgroundColor = rgb(192, 192, 192);
-        pButton.disabled = true;
+        submit.style.backgroundColor = "#8b0000";
+        submit.disabled = true;
     }
 })
+
